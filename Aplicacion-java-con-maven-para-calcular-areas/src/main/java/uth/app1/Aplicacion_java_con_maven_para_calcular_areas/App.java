d022bf6c536516dddc9fd61f985d0028ca7cadd1
DESKTOP-LF8IAPM\DELL <wencellrivera78@gmnail.com>
2023-06-23 00:07:27 -0500

    Agregando nombre
    agregue mi nombre al mensaje de bienvenida del programa

--- Aplicacion-java-con-maven-para-calcular-areas/src/main/java/uth/app1/Aplicacion_java_con_maven_para_calcular_areas/App.java
+++ Aplicacion-java-con-maven-para-calcular-areas/src/main/java/uth/app1/Aplicacion_java_con_maven_para_calcular_areas/App.java
@@ -10,7 +10,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola bienvenido a tu calculadora de áreas" );
+    	String nombre = "Wencell";
+        System.out.println( "Hola bienvenido a tu calculadora de áreas  " +nombre);
         int opcion = 0;
         
         while(opcion !=5) {
